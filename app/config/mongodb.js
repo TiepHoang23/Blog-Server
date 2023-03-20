efef64315b3497f4d04484dc9a08f11fd18b3571
TiepHoang23 <89854924+TiepHoang23@users.noreply.github.com>
2023-03-20 17:38:24 +0700

    update

--- app/config/mongodb.js
+++ app/config/mongodb.js
@@ -2,7 +2,8 @@ module.exports = {
   mongodb: {
     // connnectionString: 'mongodb+srv://admin:12345@cluster0.hdshf.mongodb.net/Grab?retryWrites=true&w=majority',
     // connnectionString: 'mongodb+srv://admin:123@cluster0.ybyop.mongodb.net/test',
-    connnectionString: 'mongodb://localhost:27017',
+    connnectionString:
+      'mongodb+srv://sa:1234@cluster0.jjg1u.mongodb.net/?retryWrites=true&w=majority',
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
